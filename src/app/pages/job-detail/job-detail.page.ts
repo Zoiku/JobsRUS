ed5b6751be6113b9933be4e46a7c32b86781e41d
“mark <mark.zoiku@ashesi.edu.gh>
2021-04-24 18:58:36 +0000

    Job Details

--- src/app/pages/job-detail/job-detail.page.ts
+++ src/app/pages/job-detail/job-detail.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { ListingsService } from '../../services/listings.service';
+import { Listing } from '../../interfaces/listing';
 @Component({
   selector: 'app-job-detail',
   templateUrl: './job-detail.page.html',
   styleUrls: ['./job-detail.page.scss'],
 })
 export class JobDetailPage implements OnInit {
+  id:number = 0;
+  listing:Listing[];
 
-  constructor() { }
+  constructor(private activatedRoute:ActivatedRoute, private listingService: ListingsService) { }
 
   ngOnInit() {
+    this.listing = this.listingService.joblistings.filter(job => job.id === parseInt(this.activatedRoute.snapshot.paramMap.get('id')));
+
+    console.log(this.listing);
   }
 
 }
